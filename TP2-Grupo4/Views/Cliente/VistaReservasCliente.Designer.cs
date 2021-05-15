2b9636f963f422125ed5a3d61b2f84ec3801b26f
saul zarate <saul.zarate@davinci.edu.ar>
2021-05-15 00:09:13 -0300

    DataGridView ajustado al ancho del contenido y bloqueo de modificacion del ancho de las columnas para los usuarios. Hecho con propiedades del DataGridView

--- TP2-Grupo4/Views/Cliente/VistaReservasCliente.Designer.cs
+++ TP2-Grupo4/Views/Cliente/VistaReservasCliente.Designer.cs
@@ -36,11 +36,13 @@ namespace TP2_Grupo4.Views
             // 
             // dgvReservaciones
             // 
+            this.dgvReservaciones.AllowUserToResizeColumns = false;
+            this.dgvReservaciones.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvReservaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReservaciones.Location = new System.Drawing.Point(12, 55);
+            this.dgvReservaciones.Location = new System.Drawing.Point(67, 105);
             this.dgvReservaciones.Name = "dgvReservaciones";
             this.dgvReservaciones.RowTemplate.Height = 25;
-            this.dgvReservaciones.Size = new System.Drawing.Size(976, 495);
+            this.dgvReservaciones.Size = new System.Drawing.Size(858, 382);
             this.dgvReservaciones.TabIndex = 0;
             this.dgvReservaciones.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -48,7 +50,7 @@ namespace TP2_Grupo4.Views
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(356, 7);
+            this.label1.Location = new System.Drawing.Point(353, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(287, 45);
             this.label1.TabIndex = 0;
