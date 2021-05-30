11735b63c854272c096d23e86021f528a0149832
SaulZarate <69607444+SaulZarate@users.noreply.github.com>
2021-05-30 15:22:28 -0300

    SQL de la base de datos en MySql

---
+++ TP2-Grupo4/Data/database.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-05-2021 a las 20:15:48
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dv-tp-plataformasdedesarrollo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alojamientos`
+--
+
+CREATE TABLE `alojamientos` (
+  `id` int(50) UNSIGNED NOT NULL,
+  `codigo` varchar(50) NOT NULL,
+  `ciudad` varchar(100) NOT NULL,
+  `barrio` varchar(100) NOT NULL,
+  `estrellas` int(2) NOT NULL,
+  `cantidadDePersonas` int(2) DEFAULT NULL,
+  `tv` tinyint(1) NOT NULL,
+  `precioPorPersona` float UNSIGNED DEFAULT 0,
+  `precioPorDia` float UNSIGNED DEFAULT 0,
+  `habitaciones` int(2) DEFAULT 0,
+  `banios` int(2) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(50) UNSIGNED NOT NULL,
+  `fechaDesde` datetime NOT NULL,
+  `fechaHasta` datetime NOT NULL,
+  `precio` double UNSIGNED NOT NULL,
+  `alojamiento_id` int(50) UNSIGNED NOT NULL,
+  `usuario_id` int(50) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(50) UNSIGNED NOT NULL,
+  `dni` varchar(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `isAdmin` tinyint(1) DEFAULT 0,
+  `isBloqueado` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alojamientos`
+--
+ALTER TABLE `alojamientos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo` (`codigo`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alojamientos`
+--
+ALTER TABLE `alojamientos`
+  MODIFY `id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
